cf9d8350ffb2f1be550cf22bdc0e6929a7cf2ed7
Jeremy Pack <rostovpack@gmail.com>
2012-11-06 17:03:25 +0000

    Split main.js into two files.
    git-svn-id: http://sandy-disaster-recovery.googlecode.com/svn/trunk@36 6201a407-fd10-26c8-02aa-e656739fd6f4

--- sandy-disaster-recovery/javascript/main.js
+++ sandy-disaster-recovery/javascript/main.js
@@ -5,51 +5,12 @@ goog.require('goog.ui.Dialog');
 goog.require('goog.ui.Dialog.ButtonSet');
 goog.require('goog.ui.Option');
 goog.require('goog.ui.Select');
+goog.require('sandy.map');
 
-var markers = [];
-var panorama;
+goog.provide('sandy.main');
 
-var layers = [
-    { kml: "https://www.google.com/maps/ms?authuser=0&vps=3&ie=UTF8&msa=0&output=kml&msid=210988455284977221384.0004cdd5426e591f0780f",
-      description: "Command Centers" },
-    { kml: "https://www.google.com/maps/ms?ie=UTF8&authuser=0&msa=0&output=kml&msid=210988455284977221384.0004cdd57d7e7e61d9c00",
-      description: "Affected Areas" },
-              ];
-
-function classifySite(site) {
-  var tags = [];
-  tags.push(site["debris_removal_only"] ? "debris_only" : "not_only_debris");
-  tags.push(site["electricity"] ? "electricity" : "no_electricty");
-  tags.push(site["standing_water"] ? "standing_water" : "no_standing_water");
-  tags.push(site["tree_damage"] ? "tree_damage" : "no_tree_damage");
-  tags.push(site["habitable"] ? "habitable" : "not_habitable");
-  tags.push(site["electrical_lines"] ? "electrial_lines" : "electrical_line_free");
-  tags.push(site["cable_lines"] ? "phone_or_cable_lines" : "phone_or_cable_line_free");
-  tags.push(site["cutting_cause_harm"] ? "trees_threaten_property" : "trees_dont_threaten_property");
-  tags.push(site["work_type"]);
-  tags.push(site["property_type"]);
-  tags.push(site["state"]);
-  return tags;
-}
-
-function refilter() {
-  var els = document.getElementsByName("filter");
-  var filters = [];
-  for (var el = 0; el < els.length; ++el) {
-    if (els[el].checked) filters.push(els[el].id);
-  }
-  for (var i = 0; i < markers.length; ++i) {
-    var include = true;
-    for (var f = 0; f < filters.length; ++f) {
-      if (markers[i].tags.indexOf(filters[f]) === -1) {
-        include = false;
-        break;
-      }
-    }
-    markers[i].setVisible(include);
-  }
-}
 var dialog;
+var panorama;
 
 // Creates and returns a <select> DOM element populated with status choices.
 // The site's current value is selected.
@@ -142,7 +103,6 @@ function AddMarker(lat, lng, site, map, infowindow) {
     icon: new google.maps.MarkerImage(siteToIconUrl(site))
   });
   marker["tags"] = classifySite(site);
-  markers.push(marker);
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent("<h2>" + site["name"] + "</h2>" + "Address: " + site["address"] + " " + site["city"] + "<br/>" + "Requests: " + site["work_requested"] + "<br/>");
     // infowindow.open(map, marker);
@@ -171,10 +131,10 @@ function AddMarker(lat, lng, site, map, infowindow) {
     dialog.getElement().style.right = "10px";
     dialog.getElement().style.bottom = "10px";
   });
+  return marker;
 }
 
-var layerObjects = [];
-function initialize() {
+sandy.main.initialize = function() {
   var myLatlng = new google.maps.LatLng(39.483351, -74.999737);
   var mapOptions = {
     zoom: 4,
@@ -183,52 +143,9 @@ function initialize() {
   };
 
   var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-  // Initialize KML layers
-  for (var i = 0; i < layers.length; ++i) {
-    var layer = new google.maps.KmlLayer({
-       clickable: true,
-       map: map,
-       preserveViewport: true,
-       suprressInfoWindows: false,
-       url: layers[i].kml,
-        });
-    layerObjects.push(layer);
-  }
-
-  var infowindow = new google.maps.InfoWindow({
-    content: ""
-  });
-  for (var i = 0; i < sites.length; ++i) {
-    var site = sites[i];
-    var lat = site["latitude"];
-    var lng = site["longitude"];
-    var good_ll = true;
-    if (isNaN(lat) || isNaN(lng)) {
-      good_ll = false;
-    }
-    if (good_ll) {
-      AddMarker(lat, lng, site, map, infowindow);
-    }
-  }
-  if (markers.length > 0) {
-    var min_lat = markers[0].getPosition().lat();
-    var max_lat = min_lat;
-    var min_lng = markers[0].getPosition().lng();
-    var max_lng = min_lng;
-    // Note that this code may completely break for those
-    // hurricane victims near the international date line.
-    for (var i = 1; i < markers.length; ++i) {
-      var ll = markers[i].getPosition();
-      min_lat = Math.min(min_lat, ll.lat());
-      min_lng = Math.min(min_lng, ll.lng());
-      max_lat = Math.max(max_lat, ll.lat());
-      max_lng = Math.max(max_lng, ll.lng());
-    }
-    map.fitBounds(new google.maps.LatLngBounds(
-        new google.maps.LatLng(min_lat - .001, min_lng - .001), 
-        new google.maps.LatLng(max_lat + .001, max_lng + .001)));
-  }
-  if (markers.length > 0) myLatlng = markers[0].getPosition();
+  // TODO(rostovpack): Set myLatLng to the location of the highest
+  // priority current site.
+  sandy.map.InitializeMap(sites, AddMarker, map);
   var panoramaOptions = {
     position: myLatlng,
     pov: {
---
+++ sandy-disaster-recovery/javascript/map.js
@@ -0,0 +1,105 @@
+goog.require('goog.dom');
+goog.require('goog.events.EventType');
+goog.require('goog.string');
+goog.require('goog.ui.Dialog');
+goog.require('goog.ui.Dialog.ButtonSet');
+goog.require('goog.ui.Option');
+goog.require('goog.ui.Select');
+
+goog.provide('sandy.map');
+
+var markers = [];
+var panorama;
+var layerObjects = [];
+var mapSites;
+
+var layers = [
+    { kml: "https://www.google.com/maps/ms?authuser=0&vps=3&ie=UTF8&msa=0&output=kml&msid=210988455284977221384.0004cdd5426e591f0780f",
+      description: "Command Centers" },
+    { kml: "https://www.google.com/maps/ms?ie=UTF8&authuser=0&msa=0&output=kml&msid=210988455284977221384.0004cdd57d7e7e61d9c00",
+      description: "Affected Areas" },
+              ];
+
+function classifySite(site) {
+  var tags = [];
+  tags.push(site["debris_removal_only"] ? "debris_only" : "not_only_debris");
+  tags.push(site["electricity"] ? "electricity" : "no_electricty");
+  tags.push(site["standing_water"] ? "standing_water" : "no_standing_water");
+  tags.push(site["tree_damage"] ? "tree_damage" : "no_tree_damage");
+  tags.push(site["habitable"] ? "habitable" : "not_habitable");
+  tags.push(site["electrical_lines"] ? "electrial_lines" : "electrical_line_free");
+  tags.push(site["cable_lines"] ? "phone_or_cable_lines" : "phone_or_cable_line_free");
+  tags.push(site["cutting_cause_harm"] ? "trees_threaten_property" : "trees_dont_threaten_property");
+  tags.push(site["work_type"]);
+  tags.push(site["property_type"]);
+  tags.push(site["state"]);
+  return tags;
+}
+
+function refilter() {
+  var els = document.getElementsByName("filter");
+  var filters = [];
+  for (var el = 0; el < els.length; ++el) {
+    if (els[el].checked) filters.push(els[el].id);
+  }
+  for (var i = 0; i < markers.length; ++i) {
+    var include = true;
+    for (var f = 0; f < filters.length; ++f) {
+      if (markers[i].tags.indexOf(filters[f]) === -1) {
+        include = false;
+        break;
+      }
+    }
+    markers[i].setVisible(include);
+  }
+}
+
+sandy.map.InitializeMap = function(currentMapSites, AddMarker, map) {
+  mapSites = currentMapSites;
+  // Initialize KML layers
+  for (var i = 0; i < layers.length; ++i) {
+    var layer = new google.maps.KmlLayer({
+       clickable: true,
+       map: map,
+       preserveViewport: true,
+       suprressInfoWindows: false,
+       url: layers[i].kml,
+        });
+    layerObjects.push(layer);
+  }
+
+  var infowindow = new google.maps.InfoWindow({
+    content: ""
+  });
+  for (var i = 0; i < mapSites.length; ++i) {
+    var site = mapSites[i];
+    var lat = site["latitude"];
+    var lng = site["longitude"];
+    var good_ll = true;
+    if (isNaN(lat) || isNaN(lng)) {
+      good_ll = false;
+    }
+    if (good_ll) {
+      markers.push(AddMarker(lat, lng, site, map, infowindow));
+    }
+  }
+  if (markers.length > 0) {
+    var min_lat = markers[0].getPosition().lat();
+    var max_lat = min_lat;
+    var min_lng = markers[0].getPosition().lng();
+    var max_lng = min_lng;
+    // Note that this code may completely break for those
+    // hurricane victims near the international date line.
+    for (var i = 1; i < markers.length; ++i) {
+      var ll = markers[i].getPosition();
+      min_lat = Math.min(min_lat, ll.lat());
+      min_lng = Math.min(min_lng, ll.lng());
+      max_lat = Math.max(max_lat, ll.lat());
+      max_lng = Math.max(max_lng, ll.lng());
+    }
+    map.fitBounds(new google.maps.LatLngBounds(
+        new google.maps.LatLng(min_lat - .001, min_lng - .001), 
+        new google.maps.LatLng(max_lat + .001, max_lng + .001)));
+  }
+  if (markers.length > 0) myLatlng = markers[0].getPosition();
+}

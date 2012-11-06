3ef36ad72bf552238423f3b79ca84a70063c38f3
Jeremy Pack <rostovpack@gmail.com>
2012-11-06 18:57:54 +0000

    Add missing files.
    git-svn-id: http://sandy-disaster-recovery.googlecode.com/svn/trunk@41 6201a407-fd10-26c8-02aa-e656739fd6f4

---
+++ sandy-disaster-recovery/print_handler.py
@@ -0,0 +1,29 @@
+# System libraries.
+import jinja2
+import os
+from google.appengine.ext import db
+
+# Local libraries.
+import base
+import site_db
+def silent_none(value):
+     if value is None:
+         return ''
+     return value
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+jinja_environment.finalize = silent_none
+template = jinja_environment.get_template('print.html')
+single_site_template = jinja_environment.get_template('single_site.html')
+class PrintHandler(base.AuthenticatedHandler):
+  """Handler to confirm and then actually delete a site."""
+  def AuthenticatedGet(self, org):
+    id = int(self.request.get('id'))
+    site = site_db.Site.get(db.Key.from_path('Site', id))
+    template_values = {"form": site,
+                       "id": id,
+                       "readonly": True}
+    self.response.out.write(template.render({
+        "single_site": single_site_template.render(template_values),
+        }))

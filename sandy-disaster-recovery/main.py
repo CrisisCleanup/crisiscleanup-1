a7bf80676eb05f1a90135898398659e17af56d0c
bruckie <bruckie@gmail.com>
2012-11-07 04:38:35 +0000

    Make all URLs work with or without / at the end. Also add /maps and /dev/maps that redirect to the "map" equivalent.
    git-svn-id: http://sandy-disaster-recovery.googlecode.com/svn/trunk@56 6201a407-fd10-26c8-02aa-e656739fd6f4

--- sandy-disaster-recovery/main.py
+++ sandy-disaster-recovery/main.py
@@ -4,7 +4,7 @@ import jinja2
 import json
 import os
 import webapp2
-from google.appengine.ext.webapp.util import run_wsgi_app
+from webapp2_extras import routes
 
 # Local libraries.
 import authentication_handler
@@ -23,9 +23,8 @@ import sites_handler
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class MapRedirectHandler(base.RequestHandler):
-  def get(self):
-    self.redirect("http://maps.google.com/?q=http://www.aarontitus.net/maps/sandy_work_orders.kmz")
+MAP_URL = "http://maps.google.com/?q=http://www.aarontitus.net/maps/sandy_work_orders.kmz"
+SPREADSHEET_URL = "https://docs.google.com/spreadsheet/ccc?key=0AhBdPrWyrhIfdFVHMDFOc0NCQjNNbmVvNHJybTlBUXc#gid=0"
 
 class LogoutHandler(base.RequestHandler):
   def get(self):
@@ -33,22 +32,29 @@ class LogoutHandler(base.RequestHandler):
                                      key.GetDeleteCookie())
     self.redirect("/authentication")
 
-class SpreadsheetRedirectHandler(base.RequestHandler):
-  def get(self):
-    self.redirect("https://docs.google.com/spreadsheet/ccc?key=0AhBdPrWyrhIfdFVHMDFOc0NCQjNNbmVvNHJybTlBUXc#gid=0")
+class Route(routes.RedirectRoute):
+  def __init__(self, *args, **kwargs):
+    # This causes a URL to redirect to its canonical version without a slash.
+    # See http://webapp-improved.appspot.com/api/webapp2_extras/routes.html#webapp2_extras.routes.RedirectRoute
+    if 'strict_slash' not in kwargs:
+      kwargs['strict_slash'] = True
+    routes.RedirectRoute.__init__(self, *args, **kwargs)
 
 app = webapp2.WSGIApplication([
-    ('/api/site', site_api_handler.SiteApiHandler),
-    ('/authentication', authentication_handler.AuthenticationHandler),
-    ('/logout', LogoutHandler),
-    ('/', SpreadsheetRedirectHandler),
-    ('/delete', delete_handler.DeleteHandler),
-    ('/dev/', form_handler.FormHandler),
-    ('/dev/map', map_handler.MapHandler),
-    ('/edit', edit_handler.EditHandler),
-    ('/import', import_handler.ImportHandler),
-    ('/map', MapRedirectHandler),
-    ('/print', print_handler.PrintHandler),
-    ('/problems', problem_handler.ProblemHandler),
-    ('/sites', sites_handler.SitesHandler)
+    Route(r'/', redirect_to=SPREADSHEET_URL, name='spreadsheet_redirect'),
+    Route(r'/api/site', site_api_handler.SiteApiHandler, 'site_api'),
+    Route(r'/authentication', authentication_handler.AuthenticationHandler,
+          'auth'),
+    Route(r'/logout', LogoutHandler, 'logout'),
+    Route(r'/delete', delete_handler.DeleteHandler, 'delete'),
+    Route(r'/dev', form_handler.FormHandler, 'dev'),
+    Route(r'/dev/map', map_handler.MapHandler, 'map'),
+    Route(r'/dev/maps', redirect_to_name='map', name='maps_redirect'),
+    Route(r'/edit', edit_handler.EditHandler, 'edit'),
+    Route(r'/import', import_handler.ImportHandler, 'import'),
+    Route(r'/map', redirect_to=MAP_URL, name='external_map_redirect'),
+    Route(r'/maps', redirect_to_name=MAP_URL, name='external_maps_redirect'),
+    Route(r'/print', print_handler.PrintHandler, 'print'),
+    Route(r'/problems', problem_handler.ProblemHandler, 'problems'),
+    Route(r'/sites', sites_handler.SitesHandler, 'sites')
 ], debug=True)

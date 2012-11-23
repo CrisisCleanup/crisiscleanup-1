8cdb64c56b79e81bce581504c90192b09770f07a
Jeremy Pack <rostovpack@gmail.com>
2012-11-23 04:14:56 +0000

    Fix printing and expor
    git-svn-id: http://sandy-disaster-recovery.googlecode.com/svn/trunk@119 6201a407-fd10-26c8-02aa-e656739fd6f4

--- sandy-disaster-recovery/site_util.py
+++ sandy-disaster-recovery/site_util.py
@@ -10,10 +10,10 @@ def SitesFromIds(comma_separated_ids, event):
   If comma_separated_ids is empty, returns all sites.
   """
   if not comma_separated_ids:
-    return [site[0] for site in site_db.GetAllCached(event, "all")]
+    return [site[0] for site in site_db.GetAllCached(event)]
   else:
     try:
       ids = [int(id) for id in comma_separated_ids.split(',')]
     except:
       return None
-    return [site[0] for site in site_db.GetAllCached(event, "all", ids)]
+    return [site[0] for site in site_db.GetAllCached(event, ids = ids)]

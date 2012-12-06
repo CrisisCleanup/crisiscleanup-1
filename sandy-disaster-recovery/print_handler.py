20383e4ecb6f4fd1fe5ebdd5301cf0af17425506
kpy <kpy@google.com>
2012-12-06 00:26:50 +0000

    Use new one-page work order template for printing.
    git-svn-id: http://sandy-disaster-recovery.googlecode.com/svn/trunk@125 6201a407-fd10-26c8-02aa-e656739fd6f4

--- sandy-disaster-recovery/print_handler.py
+++ sandy-disaster-recovery/print_handler.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 #
 # System libraries.
+import datetime
 import jinja2
 import logging
 import math
@@ -25,16 +26,18 @@ from google.appengine.ext import db
 import base
 import site_util
 
+# Only works for EST!
+LOCAL_TIME_OFFSET = datetime.timedelta(seconds=-5 * 3600)
+
 def silent_none(value):
-     if value is None:
-         return ''
-     return value
+  if value is None:
+    return ''
+  return value
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 jinja_environment.finalize = silent_none
 template = jinja_environment.get_template('print.html')
-single_site_template = jinja_environment.get_template('single_site.html')
 print_single_template = jinja_environment.get_template('print_single.html')
 
 class PrintHandler(base.AuthenticatedHandler):
@@ -43,20 +46,14 @@ class PrintHandler(base.AuthenticatedHandler):
 
   def AuthenticatedPost(self, org, event):
     sites = site_util.SitesFromIds(self.request.get('id'), event)
-    content_chunks = []
-    first_time = True
-    for site in sites:
-      template_values = {"form": site,
-                         "id": site.key().id(),
-                         "readonly": True}
-      content_chunks.append(print_single_template.render({
-          "page_break": not first_time,
-          "show_map": math.fabs(site.latitude) > 0,
-          "id": site.key().id(),
-          "form": site,
-          "single_site": single_site_template.render(template_values),
-          }))
-      first_time = False
     self.response.out.write(template.render({
-        "content": ''.join(content_chunks)
-        }))
+      'content': ''.join(print_single_template.render({
+        'page_break': i > 0,
+        'show_map': math.fabs(site.latitude) > 0,
+        'id': site.key().id(),
+        'site': site,
+        'readonly': True,
+        'current_local_time': datetime.datetime.utcnow() + LOCAL_TIME_OFFSET,
+        'request_local_time': site.request_date + LOCAL_TIME_OFFSET
+      }) for i, site in enumerate(sites))
+    }))

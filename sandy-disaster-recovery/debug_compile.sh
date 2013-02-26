0bc6fcaf56f1d713ce1e2a0eea157b6d00d454c5
Chris Wood <chris@wood.is>
2013-02-26 18:18:43 +0000

    add debug_compile.sh
    git-svn-id: http://sandy-disaster-recovery.googlecode.com/svn/trunk@195 6201a407-fd10-26c8-02aa-e656739fd6f4

---
+++ sandy-disaster-recovery/debug_compile.sh
@@ -0,0 +1,30 @@
+closure_lib/closure/bin/build/closurebuilder.py \
+  --root=closure_lib/ \
+  --root=javascript/ \
+  --namespace="sandy.main" \
+  --output_mode=compiled \
+  --compiler_flags="--externs=javascript/externs.js" \
+  --compiler_flags="--debug" \
+  --compiler_flags="--formatting=PRETTY_PRINT" \
+  --compiler_jar=closure_compiler/compiler.jar > javascript/compiled_main.js 
+
+closure_lib/closure/bin/build/closurebuilder.py \
+  --root=closure_lib/ \
+  --root=javascript/ \
+  --namespace="sandy.demo" \
+  --output_mode=compiled \
+  --compiler_flags="--externs=javascript/externs.js" \
+  --compiler_flags="--debug" \
+  --compiler_flags="--formatting=PRETTY_PRINT" \
+  --compiler_jar=closure_compiler/compiler.jar > javascript/compiled_demo.js 
+
+closure_lib/closure/bin/build/closurebuilder.py \
+  --root=closure_lib/ \
+  --root=javascript/ \
+  --namespace="sandy.form" \
+  --output_mode=compiled \
+  --compiler_flags="--externs=javascript/externs.js" \
+  --compiler_flags="--debug" \
+  --compiler_flags="--formatting=PRETTY_PRINT" \
+  --compiler_jar=closure_compiler/compiler.jar > javascript/compiled_form.js 
+

a013b5eafbb3679f9cf7c3d185bbd24a0ec7a24a
Chris Wood <chris@wood.is>
2013-03-12 17:44:30 +0000

    recover assets (from instance version 132)
    version 140
    
    git-svn-id: http://sandy-disaster-recovery.googlecode.com/svn/trunk@231 6201a407-fd10-26c8-02aa-e656739fd6f4

---
+++ sandy-disaster-recovery/assets/js/script.js
@@ -0,0 +1,17 @@
+/* Author: $hekh@r d-Ziner */
+
+(function($){
+    $(document).ready(function(){
+
+	    
+		//fixing ie/older browsers
+		$('.oldies .columns article:nth-child(4n)').css('border-right', '0');
+		$('.oldies .thumb-list li:nth-child(4n)').css('margin-right','0');
+
+	
+	}); //ready
+})(jQuery);
+
+
+
+

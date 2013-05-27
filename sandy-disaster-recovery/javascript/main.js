eb1e721e51de298f225ba126848a15a89e1045c8
andy <andy@andy-AO756.(none)>
2013-05-27 01:56:58 -0400

    removed fields with '0' from infobox

--- sandy-disaster-recovery/javascript/main.js
+++ sandy-disaster-recovery/javascript/main.js
@@ -333,7 +333,7 @@ var updateDialogForSite = function (dialog, site) {
                 details += label + ". ";
             }
         } else if (typeof site[i] == "string" && site[i].length > 0) {
-	    if (label != "Event name") {
+	    if (label != "Event name" && site[i] != "0") {
 	      details += label + ": " + site[i];
 	      if (details[details.length - 1] != ".") details += ". ";
 	    }

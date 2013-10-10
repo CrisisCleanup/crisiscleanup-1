8b815bb0011c60ddfa34dfdb1cc1b9075276a260
Chris Wood <chris@wood.is>
2013-10-10 10:59:52 +0100

    /authentication : hide form if reselect the non-option and only hit AJAX on actual change (after keypress/keyup)

--- sandy-disaster-recovery/javascript/authentication_dropdown.js
+++ sandy-disaster-recovery/javascript/authentication_dropdown.js
@@ -1,38 +1,50 @@
 $(function(){
-    // hide things 
+
+    prev_selected_event = '';  // global
+
+    // hide input rows
     $(".toggle").hide();
 
     // prepend non-option
     $("#event").prepend("<option value='' selected='selected'>Choose From Below</option>").val('');
 
     // bind events on event
-    $("#event").on('change keypress', function(){
+    $("#event").on('change keyup', function(){
+        var selected_event = $(this).val();
 
-        // clear the organization select
-        $("#organization").children().remove();
+        if (selected_event !== '' && selected_event != prev_selected_event) {
+            // clear the organization select
+            $("#organization").children().remove();
 
-        // load organizations
-        $.getJSON(
-            "/organization_ajax_handler",
-            {
-                event_name: $(this).val(), ajax: 'true'
-            },
-            function(data){
-                var options = '';
-                var temp = $('<select></select>');
-     
-                $.each(data, function(key, val) {
-                    $('<option></option>').attr('value', key).text(val).appendTo(temp);
-                });
+            // load organizations
+            $.getJSON(
+                "/organization_ajax_handler",
+                {
+                    event_name: $(this).val(), ajax: 'true'
+                },
+                function(data){
+                    var options = '';
+                    var temp = $('<select></select>');
 
-                $("#organization").children().remove();
-                temp.children().detach().appendTo($("#organization"));
-                $("#organization").prepend(
-                    "<option value='Admin' selected='selected'>Admin</option>"
-                );
-                
-                $(".toggle").show();
-            }
-        );
+                    $.each(data, function(key, val) {
+                        $('<option></option>').attr('value', key).text(val).appendTo(temp);
+                    });
+
+                    $("#organization").children().remove();
+                    temp.children().detach().appendTo($("#organization"));
+                    $("#organization").prepend(
+                        "<option value='Admin' selected='selected'>Admin</option>"
+                    );
+
+                    $(".toggle").show();
+                }
+            );
+        } else if (selected_event === '') {
+            // hide the input rows again
+            $(".toggle").hide();
+        }
+
+        // save choice
+        prev_selected_event = selected_event;
     });
 });

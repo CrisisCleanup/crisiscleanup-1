8884e8dfcc5fc0471b266574c8d50ad7be2102cf
Chris Wood <chris@wood.is>
2013-10-23 18:32:31 +0100

    Poll using AJAX to download CSVs from bulk exporter

---
+++ sandy-disaster-recovery/javascript/map_support.js
@@ -0,0 +1,52 @@
+$(function() {
+
+var pollForCSVDownload = function (filename) {
+  var downloadUrl = '/export_bulk_download?filename=' + filename;
+  $.ajax({
+    url: downloadUrl, 
+    complete: function(xhr) {
+      if (xhr.status == 200) {
+        // try again shortly
+        setTimeout(
+          function () {
+            pollForCSVDownload(filename)
+          },
+          1500
+        )
+      } else {
+        // success: redirect (after race avoidance)
+        setTimeout(
+          function() {
+            window.location = downloadUrl;
+            $('#filtered-export-btn').prop('disabled', false)
+              .attr('value', 'Download Spreadsheet (CSV)');
+          },
+          1000
+        );
+      }
+    }
+  });
+};
+
+
+// bind filtered export button click
+$('#filtered-export-btn').click(function () {
+
+  // disable button and rewrite button
+  $('#filtered-export-btn').prop('disabled', true).attr('value', 'Processing... please wait');
+
+  // request export and begin polling
+  $.ajax({
+      url: '/export_bulk',
+      data: {
+        id_list: $('.id_list').val()
+      }
+    }).done(function(data) {
+      pollForCSVDownload(data.filename);
+    });
+
+  // prevent default
+  return false;
+});
+  
+});

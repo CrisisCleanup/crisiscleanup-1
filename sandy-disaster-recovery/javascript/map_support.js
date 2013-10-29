0edf0d32fd9f01cc13fd7166fe54e9f2d2659887
Chris Wood <chris@wood.is>
2013-10-29 18:06:14 +0000

    Main map: replace gif spinner with js spinner

--- sandy-disaster-recovery/javascript/map_support.js
+++ sandy-disaster-recovery/javascript/map_support.js
@@ -35,7 +35,28 @@ $('#filtered-export-btn').click(function () {
 
   // disable & re-label button and show message
   $('#filtered-export-btn').prop('disabled', true).attr('value', 'Processing... please wait');
+
+  // create spinner
+  // (necessary because animated gifs are stopped by the change to window.location)
   $('#filtered-export-wait-message').show();
+  new Spinner({
+      lines: 13, // The number of lines to draw
+      length: 8, // The length of each line
+      width: 2, // The line thickness
+      radius: 8, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#000', // #rgb or #rrggbb or array of colors
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: true, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: 6, // Top position relative to parent in px
+      left: -48 // Left position relative to parent in px
+  }).spin($('#filtered-export-spinner')[0]);
 
   // request export and begin polling
   $.ajax({

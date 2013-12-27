45e9195277e874a83d1a830191955f15cec894a6
Chris Wood <chris@wood.is>
2013-12-27 14:34:11 +0000

    Browse Sites: fix paging and spruce list

--- sandy-disaster-recovery/sites_handler.py
+++ sandy-disaster-recovery/sites_handler.py
@@ -15,55 +15,33 @@
 # limitations under the License.
 #
 # System libraries.
-import datetime
-import json
-import logging
 import os
 from google.appengine.ext import db
 
 # Local libraries.
 import base
-import site_db
 import cgi
-import key
-import random_password
 import jinja2
 
 
-PAGE_OFFSET = 200
 jinja_environment = jinja2.Environment(
 loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 template = jinja_environment.get_template('sites.html')
 
+SITES_PER_PAGE = 200
+
+
 class SitesHandler(base.AuthenticatedHandler):
+
   def AuthenticatedGet(self, org, event):
-    page = cgi.escape(self.request.get("page"))  
-    if not page:
-        page = "0"
-    logging.debug("page = " + page)
     try:
-        page = int(page)
-    except:
-        print "error"
+        page = max(0, int(self.request.get("page", 0)))
+    except ValueError:
+        page = 0
     message = cgi.escape(self.request.get("message"))
     order = cgi.escape(self.request.get("order", "name"))
-    #if message:
-      #self.response.out.write(message + "<br /><br />")
-      
-    #self.response.out.write("<a href='/sites?order=request_date'>Order by date created</a><br />")
-    #self.response.out.write("<a href='/sites?order=name'>Order by name</a><br /><br/>")
-    #next_page =  page + 1
-    #page_link = "<a href='/sites?page=%d'>Next Page</a><br /><br/>" % next_page
-    #self.response.out.write(page_link)
-    #self.response.out.write("200 shown per page")
-    #self.response.out.write(message + "<br /><br />")
-    
-    
-    
-    
-    
     county = self.request.get("county", default_value = "NoCounty")
-    
+
     # GQL query string builder
     # escapes WHERE clause with :where_variable
     # system is throwing errors when I try to escape ORDER BY
@@ -77,30 +55,17 @@ class SitesHandler(base.AuthenticatedHandler):
     # end query string
     
     if county == "NoCounty":
-      query_string = "SELECT * FROM Site WHERE event = :event_key" + order_string +" LIMIT %s OFFSET %s" % (PAGE_OFFSET, page * PAGE_OFFSET)      
+      query_string = "SELECT * FROM Site WHERE event = :event_key" + order_string +" LIMIT %s OFFSET %s" % (SITES_PER_PAGE, page * SITES_PER_PAGE)      
       
       query = db.GqlQuery(query_string, event_key = event.key())
         
     else:
-      logging.critical(county)
-      query_string = "SELECT * FROM Site WHERE county = :county and event = :event_key " + order_string + " LIMIT %s OFFSET %s" % (PAGE_OFFSET, page * PAGE_OFFSET)
+      query_string = "SELECT * FROM Site WHERE county = :county and event = :event_key " + order_string + " LIMIT %s OFFSET %s" % (SITES_PER_PAGE, page * SITES_PER_PAGE)
       query = db.GqlQuery(query_string, where_variable = county, event_key = event.key())
       
-    #for q in query:
-      #raise Exception(q.key().id)
-    show_previous_page = False
-    if page != 0:
-      show_previous_page = True
     self.response.out.write(template.render(
     {
 	"sites_query": query,
 	"page_number": page,
-	"show_previous_page": show_previous_page,
+        "sites_per_page": SITES_PER_PAGE,
     }))
-    return
-    #for s in query:
-      #self.response.out.write(
-          #'<a href="/edit?id=%(id)s">Edit</a> '
-          #'<a href="/delete?id=%(id)s">Delete</a> - ' % {'id' : s.key().id() })
-      #self.response.out.write("%s: %s - %s<br />" %
-                              #(s.case_number, s.name, s.address))

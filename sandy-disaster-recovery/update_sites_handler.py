af4ee3ee7b0fa1a823309ddf31fc785846286c6c
andygimma <andy.n.gimma@gmail.com>
2014-01-11 20:55:16 -0500

    updating schema

--- sandy-disaster-recovery/update_sites_handler.py
+++ sandy-disaster-recovery/update_sites_handler.py
@@ -8,17 +8,19 @@ BATCH_SIZE = 100  # ideal batch size may vary based on entity size.
 import site_db
 
 def UpdateSchema(cursor=None, num_updated=0):
+    query = site_db.Site.all()
+
     if cursor:
         query.with_cursor(cursor)
 
     to_put = []
-    query = site_db.Site.all()
     for p in query.fetch(limit=BATCH_SIZE):
         # In this example, the default values of 0 for num_votes and avg_rating
         # are acceptable, so we don't need this loop.  If we wanted to manually
         # manipulate property values, it might go something like this:
         p.is_legacy_and_first_phase = True
         to_put.append(p)
+        logging.debug("end loop")
 
     if to_put:
         db.put(to_put)

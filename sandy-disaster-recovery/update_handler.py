7cc56a55b3d3010fd3fc080b78b7064abfc75b4a
andygimma <andy.n.gimma@gmail.com>
2014-03-02 12:33:13 -0500

    fixed initial errors, minor edits. Site now runs in dev server

--- sandy-disaster-recovery/update_handler.py
+++ sandy-disaster-recovery/update_handler.py
@@ -1,17 +1,11 @@
 import webapp2
-<<<<<<< HEAD
 import add_permissions_schema_update
-=======
 import update_sites_handler
->>>>>>> idmerge
 from google.appengine.ext import deferred
 
 class UpdateHandler(webapp2.RequestHandler):
     def get(self):
-<<<<<<< HEAD
         deferred.defer(add_permissions_schema_update.AddPermissionsSchemaUpdate)
         self.response.out.write('Schema migration successfully initiated.')
-=======
         deferred.defer(update_sites_handler.UpdateSchema)
         self.response.out.write('Schema migration successfully initiated.')
->>>>>>> idmerge

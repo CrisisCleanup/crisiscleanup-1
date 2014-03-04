0acf0484c8639a318d59c5061652d93cf736b805
andygimma <andy.n.gimma@gmail.com>
2014-03-04 14:11:00 -0500

    remove organizations property from IncidentDefinitionForm. No longer necessary

--- sandy-disaster-recovery/models/incident_definition.py
+++ sandy-disaster-recovery/models/incident_definition.py
@@ -212,7 +212,6 @@ class IncidentDefinitionForm(model_form(IncidentDefinition)):
   message = "Local Admin Last Name must be between 1 and 100 characters")])
   local_admin_title = TextField('Local Admin Title', [wtforms.validators.Length(min = 1, max = 100,
   message = "Local Admin Title must be between 1 and 100 characters")])
-  #local_admin_organization = wtforms.fields.SelectField("Organization (by Incident)", choices = organizations_array)
   local_admin_email = TextField('Local Admin Email', [wtforms.validators.Email(message=u'Invalid email address.')])
   local_admin_cell_phone = TextField('Local Admin Cell Phone', [phone_validator])
 

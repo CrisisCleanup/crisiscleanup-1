4b0f93deebb851ed92230f5cf8e8c2cb2342e669
nicolaszanghi <nicolas.zanghi@gmail.com>
2014-06-13 15:10:20 +0200

    messaging: catch & log all exceptions to prevent blowing up requests due to email

--- sandy-disaster-recovery/messaging.py
+++ sandy-disaster-recovery/messaging.py
@@ -150,19 +150,23 @@ def send_email_by_service(to, subject, body, cc=None, bcc=None, html_body=None):
     aws_sender_address = get_aws_ses_default_system_email_address()
 
     # send by AWS or fall back to GAE
-    if can_send_by_aws_ses(aws_ses_region, aws_ses_access_key_id, aws_ses_secret_access_key, aws_sender_address):
-        return send_email_via_aws_ses(
-            aws_sender_address,
-            to, subject, body, cc=cc, bcc=bcc, html_body=html_body,
-            aws_ses_region=aws_ses_region,
-            aws_ses_access_key_id=aws_ses_access_key_id,
-            aws_ses_secret_access_key=aws_ses_secret_access_key
-        )
-    else:
-        return send_email_via_appengine(
-            gae_sender_address,
-            to, subject, body, cc=cc, bcc=bcc, html_body=html_body
-        )
+    # catch & log all exceptions to prevent blowing up requests due to email
+    try:
+        if can_send_by_aws_ses(aws_ses_region, aws_ses_access_key_id, aws_ses_secret_access_key, aws_sender_address):
+            return send_email_via_aws_ses(
+                aws_sender_address,
+                to, subject, body, cc=cc, bcc=bcc, html_body=html_body,
+                aws_ses_region=aws_ses_region,
+                aws_ses_access_key_id=aws_ses_access_key_id,
+                aws_ses_secret_access_key=aws_ses_secret_access_key
+            )
+        else:
+            return send_email_via_appengine(
+                gae_sender_address,
+                to, subject, body, cc=cc, bcc=bcc, html_body=html_body
+            )
+    except:
+        logging.exception("Exception caused generating email.")
 
 
 def friendly_email_address(contact):

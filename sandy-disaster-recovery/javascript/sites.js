2b30efb2032c11d0590e162345a41149d619dcb2
andygimma <andy.n.gimma@gmail.com>
2015-06-03 15:42:03 -0400

    testing map loading

--- sandy-disaster-recovery/javascript/sites.js
+++ sandy-disaster-recovery/javascript/sites.js
@@ -108,7 +108,7 @@ sandy.sites.loadSitesBatch = function (sites_status, page, url, callBack) {
                 sites.push(newSites[i]);
             }
 
-            if (sites.length > 95) {
+            if (sites.length > 50) {
                 empty = false;
             }
 

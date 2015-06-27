18a35e3db0a945288e1b286d3226da5fbec987b9
Andy Gimma <andy.n.gimma@gmail.com>
2015-06-27 16:10:18 -0500

    Changes to import for Houston Only

--- sandy-disaster-recovery/upload_handler.py
+++ sandy-disaster-recovery/upload_handler.py
@@ -173,6 +173,7 @@ def edit_references_if_none_exists(site, data):
         c = float(str(d))
         b = int(c)
         org = organization.Organization.get_by_id(b)
+        logging.info(org)
         if site.case_number == "R1057":
           logging.info(site.name)
           logging.info(site.claimed_by)

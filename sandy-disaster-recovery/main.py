36432dcaaa11ca5b17b2befc090d8abec5239004
Andy Gimma <andy.n.gimma@gmail.com>
2015-07-11 11:36:15 -0500

    .

--- sandy-disaster-recovery/main.py
+++ sandy-disaster-recovery/main.py
@@ -239,7 +239,7 @@ app = webapp2.WSGIApplication([
     Route(r'/task/export-all-events', export_bulk_handler.ExportAllEventsHandler, 'export_all_events'),
     Route(r'/task/crunch-all-events-stats', stats.CrunchAllStatisticsHandler, 'crunch_all_events_stats'),
     Route(r'/update_handler', update_handler.UpdateHandler, 'update_handler'),
-    # Route(r'/api/migration', migration_handler.MigrationHandler, 'migration_handler'),
+    Route(r'/api/migration', migration_handler.MigrationHandler, 'migration_handler'),
 
     # dev/testing
     ###Route(r'/test-email', messaging.EmailTestHandler, 'email_test'),

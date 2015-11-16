3261d022aed627fdbf84e663f1766367bd4e6696
andygimma <andy.n.gimma@gmail.com>
2015-11-16 15:25:47 -0500

    add env

---
+++ sandy-disaster-recovery/env.py
@@ -0,0 +1,2 @@
+SALT = "234kl2;pisdf9079087*(&()*YIUGT(87e6q2uhqphp<?>LU^()_)HJK##DGJKJKNKJhk@*($#PIWROIlkuoiu324\
+	    jksl98sd7f98sdf(*&h342oi*)&IOUIH0870938234j90r80(*)(*)(*)(*j324j34i098IjOIUOIUhjl0(*)89034"

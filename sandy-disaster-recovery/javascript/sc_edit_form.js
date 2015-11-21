c57573641e1310cbbdedc890419602b34f2c35f0
andygimma <andy.n.gimma@gmail.com>
2015-11-20 20:55:10 -0500

    add logs

--- sandy-disaster-recovery/javascript/sc_edit_form.js
+++ sandy-disaster-recovery/javascript/sc_edit_form.js
@@ -1,6 +1,8 @@
 $(function() {
 	$.get( "/api/site_ajax", { id_param: "{{site_id}}" } )
 	  .done(function( data ) {
+	  	console.log("start ajax data");
 	    console.log(data );
+	    console.log("end ajax data");
 	  });
 });
